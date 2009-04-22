b90bb91001b9564284ea0626b82efaba6257a4a7
j.metlik <j.metlik@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-04-22 12:13:56 +0000

    ha

--- trunk/two/src/array_extension.rb
+++ trunk/two/src/array_extension.rb
@@ -1,16 +1,33 @@
 class Array
 
-	def select_first(key, value)
-		puts key
-		@inre_key = key
-		@inre_value = value
+	def select_first(args)
+		stop = false
 		self.each do |p|
-			puts @inre_key
-			puts p.name
-			#puts p.@inre_x
-			#if p.x.length == y
-			#	puts p.x
-			#end
+			args.each_pair do |key, value|
+				value.each do |v|
+					if p.send(key) == v
+						puts p.name
+						puts p.age
+						stop = true
+					end
+				end
+			end
+			if stop
+				break
+			end
+		end
+	end
+	
+	def select_all(args)
+		self.each do |p|
+			args.each_pair do |key, value|
+				value.each do |v|
+					if p.send(key) == v
+						puts p.name
+						puts p.age
+					end
+				end
+			end
 		end
 	end
 end
