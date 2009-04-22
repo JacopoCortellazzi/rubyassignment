563749450302ad005a04d48c2810a0c41070dc26
linus.harling <linus.harling@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-04-22 13:08:39 +0000

    funkis, metoder select_first och select_single

--- one/src/array_extension.rb
+++ one/src/array_extension.rb
@@ -1,15 +1,37 @@
 class Array
 	def select_first(args)
-		puts args.class
-		self.each do |e|
-			args.each_pair do |key, value|
-				if e.send(key) == value
-					puts e.name
-				end
+		puts args
+		puts args.size
+		if args.length == 1 
+			select_single(args)
+		elsif args.length == 2
+			if args[:interval].length == 2
+				select_single(args[:name] => (args[:interval][:min] .. args[:interval][:max]).to_a)
+			elsif args[:interval][:max] != nil
+				select_single(args[:name] => (0 .. args[:interval][:max]).to_a)
 			end
+		else
+			throw error
 		end
 	end
-	
+
+	def select_single(args)
+		@key = args.keys[0]
+		@value = args[@key]
+		self.each do |e|
+			if @value.class == Array
+				@value.each do |val|
+					if e.send(@key) == val
+						return e
+						break
+					end
+				end
+			elsif e.send(@key) == @value
+				return e
+			end
+		end	
+	end
+
 	def select_all
 	end
 end
