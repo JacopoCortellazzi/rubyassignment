7a41bf4d425c59c82b5d23e54897ff4672a720df
j.metlik <j.metlik@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-04-22 13:22:09 +0000

    h

--- trunk/two/src/array_extension.rb
+++ trunk/two/src/array_extension.rb
@@ -1,19 +1,42 @@
 class Array
-
+	
 	def select_first(args)
-		stop = false
-		self.each do |p|
-			args.each_pair do |key, value|
-				value.each do |v|
-					if p.send(key) == v
-						puts p.name
-						puts p.age
-						stop = true
+	
+		#KOLLA aarGS
+		puts args
+		if args.size == 1
+	
+			stop = false
+			self.each do |p|
+				args.each_pair do |key, value|
+					value.each do |v|
+						if p.send(key) == v
+							puts p.name
+							puts p.age
+							stop = true
+						end
 					end
 				end
+				if stop
+					break
+				end
 			end
-			if stop
-				break
+		else
+			attribute = args.fetch(:name)
+			interval = args.fetch(:interval)
+			puts interval
+			max = interval.fetch(:max)
+			min = interval.fetch(:min)
+			puts max,min
+			puts max.class
+			self.each do |p|
+				if p.send(attribute) && p.send(attribute)>min && p.send(attribute) < max
+					puts p.name,p.age
+					stop = true
+				end
+				if stop
+					break
+				end
 			end
 		end
 	end
