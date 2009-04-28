2b8f7aee5da4f51f5e0c4513b07ca09b5dcde24d
linus.harling <linus.harling@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-04-28 12:00:36 +0000

    select_all och code_generation ny

--- one/src/array_extension.rb
+++ one/src/array_extension.rb
@@ -3,36 +3,57 @@ class Array
 		#puts args
 		#puts args.size
 		if args.length == 1 
-			select_single(args)
+			select_single(:first, args)
 		elsif args.length == 2
 			if args[:interval].length == 2
-				select_single(args[:name] => (args[:interval][:min] .. args[:interval][:max]).to_a)
+				select_single(:first, args[:name] => (args[:interval][:min] .. args[:interval][:max]).to_a)
 			elsif args[:interval][:max] != nil
-				select_single(args[:name] => (0 .. args[:interval][:max]).to_a)
+				select_single(:first, args[:name] => (0 .. args[:interval][:max]).to_a)
 			end
 		else
 			throw error
 		end
 	end
 
-	def select_single(args)
-		key = args.keys[0]
-		value = args[key]
+	def select_single(*args)
+		p args
+		key = args[1].keys[0]
+		value = args[1][key]
+		retarr = []
 		self.each do |e|
 			if value.class == Array
 				value.each do |val|
 					if e.send(key) == val
-						return e
-						break
+						if args[0] == :first
+							return e
+						elsif args[0] == :all
+							retarr<<e
+						end
 					end
 				end
 			elsif e.send(key) == value
-				return e
+				if args[0] == :first
+					return e
+				elsif args[0] == :all
+					retarr<<e
+				end
 			end
-		end	
+		end
+		return retarr
 	end
 
-	def select_all
+	def select_all(args)
+		if args.length == 1 
+			select_single(:all, args)
+		elsif args.length == 2
+			if args[:interval].length == 2
+				select_single(:all, args[:name] => (args[:interval][:min] .. args[:interval][:max]).to_a)
+			elsif args[:interval][:max] != nil
+				select_single(:all, args[:name] => (0 .. args[:interval][:max]).to_a)
+			end
+		else
+			throw error
+		end
 	end
 	
 	def method_missing(method, *args)
@@ -40,6 +61,11 @@ class Array
             select_first(attribute[1].to_sym => args)
         elsif attribute = method.to_s.match(/^select_first_where_(.*)_is_in$/)
             select_first(:name => attribute[1].to_sym, :interval => {:min => args[0], 
+                            :max => args[1]})
+        elsif attribute = method.to_s.match(/^select_all_where_(.*)_is$/)
+            select_all(attribute[1].to_sym => args)
+        elsif attribute = method.to_s.match(/^select_all_where_(.*)_is_in$/)
+            select_all(:name => attribute[1].to_sym, :interval => {:min => args[0], 
                             :max => args[1]})
         end
     end
