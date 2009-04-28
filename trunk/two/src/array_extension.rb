80a3a79d4854e1c577d65ded0f725e791a2020e9
natta.sandstrom <natta.sandstrom@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-04-28 12:37:32 +0000

    del 1 klar

--- trunk/two/src/array_extension.rb
+++ trunk/two/src/array_extension.rb
@@ -23,7 +23,7 @@ class Array
       if(interval.has_key?(:min))
         min = interval.fetch(:min)
         self.each do |p|
-          if p.send(attribute) && p.send(attribute)>min && p.send(attribute) < max
+          if p.send(attribute) && p.send(attribute)>=min && p.send(attribute) <= max
             puts p.name,p.age
             stop = true
           end
@@ -33,7 +33,7 @@ class Array
         end
       else
         self.each do |p|
-          if p.send(attribute) && p.send(attribute) < max
+          if p.send(attribute) && p.send(attribute) <= max
             puts p.name,p.age
             stop = true
           end
@@ -63,23 +63,38 @@ class Array
       if(interval.has_key?(:min))
         min = interval.fetch(:min)
         self.each do |p|
-          if p.send(attribute) && p.send(attribute)>min && p.send(attribute) < max
+          if p.send(attribute) && p.send(attribute) >= min && p.send(attribute) <= max
             puts p.name,p.age
           end
         end
       else
         self.each do |p|
-          if p.send(attribute) && p.send(attribute) < max
+          if p.send(attribute) && p.send(attribute) <= max
             puts p.name,p.age
           end
         end
       end
     end
   end
-  
+
   def method_missing(method_name, *args)
-    puts method_name
+    method_name = method_name.to_s
+    if(method_name.match(/select_first_where_(.*)_is$/) )
+      select_first($1.to_sym => args)
+    elsif(method_name.match(/select_first_where_(.*)_is_in/))
+      if(args.size  > 1)
+        select_first(:name => $1.to_sym, :interval => {:min => args[0], :max => args[1]})
+      else
+        select_first(:name => $1.to_sym, :interval => {:max => args[0]}) 
+      end
+    elsif(method_name.match(/select_all_where_(.*)_is$/) )
+      select_all($1.to_sym => args)
+    elsif(method_name.match(/select_all_where_(.*)_is_in/))
+      if(args.size > 1)
+        select_all(:name => $1.to_sym, :interval=> {:min => args[0], :max => args[1]})
+      else
+        select_all(:name => $1.to_sym, :interval => {:max => args[0]}) 
+      end
+    end
   end
-  
-  
 end
