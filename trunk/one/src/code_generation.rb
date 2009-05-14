37e73d741c90271641eb70673984f5bca3a14047
paulsson.magnus <paulsson.magnus@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-05-14 11:38:00 +0000

    kkkk

--- trunk/one/src/code_generation.rb
+++ trunk/one/src/code_generation.rb
@@ -2,20 +2,39 @@
 
 class MyDSL
     
-    def loadFile( filename )
-        title = /title\s*:(.*)\n/
-        file = File.open( filename, "r" )
-        while( line = file.gets )
-            if tmp = line.match( title )
-                makeClass( tmp[1] )
-            end
-        end
-        file.close   
-	end
-	
-	def makeClass( args )
-	    @myClass = Object.const_set( args, Class.new )
-        puts @myClass
-	end
-
+    def self.load( filename )
+        dsl = new
+        dsl.instance_eval( File.read( filename ) )
+        dsl
+    end
+    
+    def title( sym )
+        @newClass = Object.const_set( sym, Class.new )
+        puts @newClass
+    end
+    
+    def attribute( sym, args )
+        @newClass.class_eval( "attr_accessor #{sym}" )
+    end
+    
+    #def method_missing( sym, args )
+    #end
 end
+
+#    def loadFile( filename )
+#        title = /title\s*:(.*)\n/
+#        file = File.open( filename, "r" )
+#        while( line = file.gets )
+#            if tmp = line.match( title )
+#                makeClass( tmp[1] )
+#            end
+#        end
+#        file.close   
+#	end
+	
+#	def makeClass( args )
+#	    @myClass = Object.const_set( args, Class.new )
+#        puts @myClass
+#	end
+#
+#end
