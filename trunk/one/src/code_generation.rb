e18fdcfc64c72f41b33c30c6abf8e2653d760fe8
paulsson.magnus <paulsson.magnus@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-05-14 10:44:40 +0000

    trasig kod

--- trunk/one/src/code_generation.rb
+++ trunk/one/src/code_generation.rb
@@ -1,8 +1,23 @@
 # code_generation.rb
 
-module Model
+#module Model
 
-	def generate(path)
+	#def generate( path )
+
+	#end
+
+class MyDSL
+	
+	def self.load( filename )
+	    title = /title\s*:(.*)\n/
+	    file = File.open( filename, "r" )
+	    #while(line = file.gets)
+	    file.each { |line| do if line.match( title ) makeClass( title[1] ) }
+	end
+	
+	def makeClass( args )
+	    @myClass = Object.const_set( args, Class.new )
+        puts @myClass
 	end
 
 end
