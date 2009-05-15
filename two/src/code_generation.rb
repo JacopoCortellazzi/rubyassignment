80d63c67a7a4e978482ef24bc42571fe90c9f08f
j.metlik <j.metlik@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-05-15 14:44:14 +0000

    kossan mu

--- two/src/code_generation.rb
+++ two/src/code_generation.rb
@@ -5,6 +5,7 @@ class CodeGenerator
     attributeReg = /\s*attribute\s*:(.*),\s*(.*)\n*/  
     constraintReg = /\s*constraint\s*:(.*),\s*'[a-z A-Z]*\.*(.*)'\n*/  
     file = File.new(args, "r")
+    @constraints = Array.new
     while(line = file.gets)
       if(line.match(titleReg))
         createNewClass($1)
@@ -14,37 +15,80 @@ class CodeGenerator
       end
       if(line.match(constraintReg))
         addConstraint($1 => $2)
+        @constraints << {$1 => $2}
       end
     end
     file.close
+    puts @constraints
+    
+
+
+        
   end
 
   def createNewClass(args)
     @myClass = Object.const_set(args,Class.new)
+    createMethod()
   end
 
   def createNewAttribute(args)
-    @myClass.class_eval("attr_accessor:#{args.keys}")
+    set_attr_reader("#{args.keys}")
+    set_attr_writer("#{args.keys}","#{args.values}")
   end
-
-  def addConstraint(args)
-    # key = args.keys.to_s
-    # value = args.values
-    alias_method :createNewAttribute, :newMethod 
-    def newMethod(args)
-      if(value.to_s.eql?("!= nil"))
-        if(key != nil)
-          createNewAttribute(args.keys)
-          puts "OK"
-        end
-      end
-      # if(value.to_s.eql?("size > 0"))
-      # end
-      # if(value.to_s.eql?("=~ /^[A-Z]/"))
-      # end
-      # puts "Value: ",value
-      # if(value.to_s.eql?(">= 0"))
-      # end
+  
+  #alias_method :old_attr_writer, :attr_writer
+  def set_attr_writer(sym, type)
+      @myClass.class_eval %{def #{sym}=(val)
+                    if(val.kind_of? #{type} && self.checkConstraint(#{sym}z, val))
+                      @#{sym} = val
+                    else
+                      raise "Type Error"
+                    end
+                  end}    
+  end
+  
+  def set_attr_reader(syms)
+    syms.each do|sym|
+      @myClass.class_eval %{def #{sym}
+                    @#{sym}
+                  end}
     end
   end
+
+  def checkConstraint(attribute, value_to_be_checked)
+    valid = true
+    @constraints.each do |p|
+      p.each_pair do |key, value|
+        key.each do |k|
+          if(k == attribute)
+            if(value.to_s.eql?("!= nil"))
+              if(!value_to_be_checked != nil)
+                valid = false
+                break
+              end
+            end
+            if(value.to_s.eql?("size > 0"))
+              if(!value_to_be_checked.size > 0)
+                valid = false
+                break
+              end
+            end
+            if(value.to_s.eql?("=~ /^[A-Z]/"))
+              if(!value_to_be_checked.match(/[A-Z]/))
+                valid = false
+                break
+              end
+            end
+            if(value.to_s.eql?(">= 0"))
+              if(!value_to_be_checked >= 0)
+                valid = false
+                break
+              end
+            end
+          end
+        end
+      end
+    end   
+  end
+
 end
