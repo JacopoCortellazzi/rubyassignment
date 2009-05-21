da4f434210628c78768c29b4b6e66fd70cf09586
natta.sandstrom <natta.sandstrom@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-05-21 10:07:08 +0000

    Jippi

--- two/src/code_generation.rb
+++ two/src/code_generation.rb
@@ -4,56 +4,42 @@ class CodeGenerator
     titleReg = /title\s*:(.*)\n/
     attributeReg = /\s*attribute\s*:(.*),\s*(.*)\n*/  
     constraintReg = /\s*constraint\s*:(.*),\s*'[a-z A-Z]*\.*(.*)'\n*/  
-    @constraints = Array.new
+    #@constraints = Array.new
     file = File.new(args, "r")
     while(line = file.gets)
-      if(line.match(constraintReg))
-        @constraints<<{$1 => $2}
-      end
-    end
-    file.close
-    file2 = File.new(args, "r")
-    while(line = file2.gets)
       if(line.match(titleReg))
         createNewClass($1)
       end
       if(line.match(attributeReg))
         createNewAttribute($1 => $2)
       end
+      if(line.match(constraintReg))
+        createConstraints($1 => $2)
+        #@constraints<<{$1 => $2}
+      end
     end
-    file2.close  
+    file.close  
     makeConstraintsMethod()
   end
 
   def createNewClass(args)
-    def call_block
-      tmp = @constraints
-      yield
-    end
     @myClass = Object.const_set(args,Class.new)
-    @myClass.class_eval %{ 
-      attr_accessor :const
-      def create_block
-      
-      CodeGenerator.new.call_block(&block)
-      def initialize
-        instance_variable_set(:@const, yield)
-        instance_variable_get(:@const)
-      end
-    }
   end
 
   def createNewAttribute(args)
     set_attr_reader("#{args.keys}")
     set_attr_writer("#{args.keys}","#{args.values}")
+    @myClass.class_eval %{class_variable_set :@@const, Array.new}
+  end
+
+  def createConstraints(args)
+    @myClass.class_eval %{@@const << args} 
   end
 
   def set_attr_writer(sym, type)
     @myClass.class_eval %{def #{sym}=(val)
-    puts val
     if(val.kind_of? #{type})
-      puts "Value är av samma typ"
-      if(checkConstraint(#{sym}, val))
+      if(checkConstraint(:#{sym}, val))
         @#{sym} = val
       else
         raise "Value did match not constraints"
@@ -76,39 +62,38 @@ def makeConstraintsMethod()
 
   @myClass.class_eval %{def checkConstraint(attribute, value_to_be_checked)
     valid = true
-    puts @const
-    @const.each do |p|
+    @@const.each do |p|
       p.each_pair do |key, value|
         key.each do |k|
-          if(k == attribute)
-            if(value.to_s.eql?("!= nil"))
-              if(!value_to_be_checked != nil)
+          if(k.to_s == attribute.to_s)
+            if(value.to_s.eql?("! = nil"))
+              if(!(value_to_be_checked != nil))
                 valid = false
                 break
               end
             end
             if(value.to_s.eql?("size > 0"))
-              if(!value_to_be_checked.size > 0)
+              if(!(value_to_be_checked.size > 0))
                 valid = false
                 break
               end
             end
             if(value.to_s.eql?("=~ /^[A-Z]/"))
-              if(!value_to_be_checked.match(/[A-Z]/))
+              if(!(value_to_be_checked.match(/[A-Z]/)))
                 valid = false
                 break
               end
             end
             if(value.to_s.eql?(">= 0"))
-              if(!value_to_be_checked >= 0)
+              if(!(value_to_be_checked >= 0))
                 valid = false
-                break
               end
             end
           end
         end
       end
-    end 
+    end
+    return valid 
   end}
 end  
 end
