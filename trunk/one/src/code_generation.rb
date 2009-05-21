78b84348ca252e59feed1a8ccd4f46ccc2a82419
linus.harling <linus.harling@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-05-21 14:11:12 +0000

    fixy trixy

--- trunk/one/src/code_generation.rb
+++ trunk/one/src/code_generation.rb
@@ -2,41 +2,72 @@
 
 #require 'dslhelper_attr_writer'
 
-class MyDSL
+class Model
     
-    def self.load(filename)
+    def self.generate(filename)
         dsl = new
         dsl.instance_eval(File.read(filename))
         dsl
+		  #return dsl.newClass
     end
+
+#	def self.generate(filename)
+#	 	self.instance_eval(File.read(filename))
+#	end
     
     def title(sym)
 		@newClass = Object.const_set(sym, Class.new)
+		@newClass.class_eval %{require 'yaml'
+									def initialize(args)
+										args.each do |arg|
+											self.method("#\{arg[0]\}=").call(arg[1])
+										end
+									end
 
+									def self.load_from_file(filename)
+										arr = YAML::load(File.open(filename)).values[0]
+										ret = Array.new
+										arr.each do |n|
+											begin
+												obj = self.new(n)
+												ret << obj
+											rescue
+											end
+										end
+										return ret
+									end
+
+									def self.method_missing(method, *args)
+										p method
+										p args
+										return nil
+									end
+									}
 		puts @newClass
     end
     
     def attribute(sym, args)
-        set_attr_writer(sym)
-        set_attr_reader(sym)
-        @newClass.class_eval %{class_variable_set :@@#{sym}_constraints, Array.new}
+      set_attr_writer(sym)
+      set_attr_reader(sym)
+		#@newClass.class_eval %{instance_variable_set :@#{sym}, nil}
+		@newClass.class_eval %{class_variable_set :@@#{sym}_constraints, Array.new}
+
     end
 
    def set_attr_writer(sym)
         @newClass.class_eval %{def #{sym}= (val)
-                        p "attr_writer for #{sym}"
 		  						old_#{sym} = @#{sym}
 								@#{sym} = val
-		  						p #{sym}.class
-								p val.class
 								if @@#{sym}_constraints.nil?
 									p "no constraints"
-								else p @@#{sym}_constraints.to_s
+								else 
 									@@#{sym}_constraints.each do |c|
-										#p "constraint: "+c+", value: "+val.to_s+", sym: "+#{sym}.to_s
-										if !eval(c)
+										if !eval(c) || @#{sym} == nil
 											@#{sym} = old_#{sym}
-											raise "Value not allowed: "+val.to_s+" rule "+c
+											#return @#{sym}
+											raise "Value out of range: "+val.to_s+" is not in "+c.to_s
+										else
+											return @#{sym}
 										end
 									end
 								end
@@ -46,15 +77,12 @@ class MyDSL
    
    def set_attr_reader(sym)
         @newClass.class_eval %{def #{sym}
-                        p "attr_reader for #{sym}"
                         @#{sym}
                      end}
     end
 
 	def constraint(sym, args)
-		@newClass.class_eval %{	p "adding constraint "+sym.to_s+" "+%(#{args})
-											@@#{sym}_constraints << %(#{args})
-		}
+		@newClass.class_eval %{@@#{sym}_constraints << %(#{args})}
 	end
 
 end
