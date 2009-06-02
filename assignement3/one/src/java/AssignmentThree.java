bca7592c8dce46a4dd80a13d62572566107341f1
paulsson.magnus <paulsson.magnus@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-06-02 08:02:41 +0000

    Refactoring

--- assignement3/one/src/java/AssignmentThree.java
+++ assignement3/one/src/java/AssignmentThree.java
@@ -3,65 +3,67 @@ import java.util.*;
 import java.lang.*;
 
 class AssignmentThree {
+    private String dictionary = "../doc/dictionary.txt";
+    private String phonenumbers = "../doc/phonenumbers.txt";
     private ArrayList<String> words = new ArrayList<String>();
     private ArrayList<String> numbers = new ArrayList<String>();
-    private HashMap<String, String> wordsByNumbers = new HashMap<String, String>();
-    private String[] combinations = {"e","jnq","rwx","dsy","ft","am","civ",
-                                                            "bku","lop","ghz"};
+    private HashMap<String, String> wordsByNumbers = 
+                                                 new HashMap<String, String>();
+    private String[] combinations = { "e","jnq","rwx","dsy","ft","am","civ",
+                                                            "bku","lop","ghz" };
     public AssignmentThree () {
         readFromFiles();
     }
     
     public void readFromFiles() {
         try {
-            BufferedReader brWords = new BufferedReader( new FileReader(
-                                                    "../doc/dictionary.txt") );
+            BufferedReader brWords = new BufferedReader( new FileReader
+                                                               ( dictionary ) );
             while ( brWords.ready() ) {
                 String line = brWords.readLine().toLowerCase();
                 words.add( line );
                 wordsByNumbers.put(line, (matchWordToNumbers( line )));
-
             }
-            BufferedReader brNumbers = new BufferedReader( new FileReader(
-                                                "../doc/phonenumbers.txt") );
+            BufferedReader brNumbers = new BufferedReader( new FileReader
+                                                             ( phonenumbers ) );
             while ( brNumbers.ready() ) {
                 numbers.add( brNumbers.readLine() );
             }
             brWords.close();
             brNumbers.close();
-        } catch (IOException ie) {
+        } catch ( IOException ie ) {
             ie.printStackTrace();
-        } catch (NullPointerException ne) { 
-            System.out.println("Krasch! ");
+        } catch ( NullPointerException ne ) { 
+            System.out.println( "Krasch! " );
             ne.printStackTrace();
         }
         //Skiver ut hashMappen
-        System.out.println(wordsByNumbers.toString());   
+        System.out.println( wordsByNumbers.toString() );   
     }
     
     public String matchWordToNumbers( String word ) {
         String tmp = "";
         for( int i = 0; i < word.length(); i++ ) {
             for( int y = 0; y < combinations.length ; y++ ) {
-                if( combinations[y].contains( 
-                                    String.valueOf(word.charAt(i)) ) )
-                    tmp += Integer.toString(y);
+                if( combinations[y].contains( String.valueOf
+                                                        ( word.charAt( i ) ) ) )
+                    tmp += Integer.toString( y );
             }
         }
         return tmp;   
     }
     
     public void encodeNumber( String num ) {
-        
+        //foreach key in wordsByNumbers
         for ( String n : wordsByNumbers.keySet() ) {
-            if( num.equals(wordsByNumbers.get(n)) )
-                System.out.println(num+" : "+n);
+            if( num.equals( wordsByNumbers.get( n ) ) )
+                System.out.println( num+" : "+n );
         }
     }
 
     public static void main( String[] args ) {
         AssignmentThree app = new AssignmentThree();
         System.out.println();
-        app.encodeNumber("51");
+        app.encodeNumber( "51" );
     }
 }
