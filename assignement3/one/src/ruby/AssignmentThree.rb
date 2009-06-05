a7c1e28883dc353b4dd860938a5a2c183cd6d385
paulsson.magnus <paulsson.magnus@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-06-05 10:09:03 +0000

    Ruby fungerar och Nattas musiksmak suger

--- assignement3/one/src/ruby/AssignmentThree.rb
+++ assignement3/one/src/ruby/AssignmentThree.rb
@@ -10,7 +10,7 @@ class AssignmentThree
     def readNumbers
         @numbers = []
         f = File.open( "../doc/tele.txt", "r" )
-        f.each do |line|
+        f.each do | line |
             @numbers << line.strip.to_s
         end
     end
@@ -19,43 +19,44 @@ class AssignmentThree
         @words = {}
         f = File.open( "../doc/dict.txt", "r" )
         f.each do | line |
-            @words[line.strip.downcase] = matchWordToNumber( line.downcase )
+            @words[ line.strip.downcase ] = matchWordToNumber( line.downcase )
         end
     end
     
     def matchWordToNumber( word )
-        tmp = ""
+        current = ""
         @combinations = ["e", "jnq", "rwx", "dsy", "ft", "am", "civ", "bku", 
                                                                   "lop", "ghz"]
         word.each_char do | char |
             @combinations.each do | s |
                 if( s.include? char )
-                    tmp += @combinations.index( s ).to_s
+                    current += @combinations.index( s ).to_s
                 end
             end
         end
-        return tmp
+        return current
     end
     
-    def startEncoding( num )
-        encodeNumber(num, "")
+    def startEncoding
+        @numbers.each do | number |
+            encodeNumber( number, "", number )
+        end
     end
     
-    def encodeNumber( num, result )
-        if(num.size == 0)
-            puts result
+    def encodeNumber( num, result, original )
+        if( num.size == 0 )
+            puts "#{original}: #{result}"
         end
         x = 1
         substring = ""
-        restOfNum = ""
-        unless x > num.size
-            substring = num.slice( 0, x+=1 )
+        num.size.times do
+            substring = num.slice( 0, x )
             @words.each_pair do | key ,value |
-                if( substring == key )
-                    restOfNum = num.sub( substring, "" )
-                    encodeNumber( restOfNum, result+key+" " )
+                if( substring == value )
+                    encodeNumber( num.sub( substring, "" ), result+key+" ", original )
                 end
             end
+            x += 1
         end
     end
 end
