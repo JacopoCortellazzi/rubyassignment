0d7289f6b0a50ff3c1ead6b7932b4d82054e0d95
j.metlik <j.metlik@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-08-17 12:49:03 +0000

    klar

--- two/src/code_generation.rb
+++ two/src/code_generation.rb
@@ -1,129 +1,100 @@
-require "yaml"
+require 'yaml'
+
 class Model
-
-  def loadFile(args)
-    titleReg = /title\s*:(.*)\n/
-    attributeReg = /\s*attribute\s*:(.*),\s*(.*)\n*/  
-    constraintReg = /\s*constraint\s*:(.*),\s*'[a-z A-Z]*\.*(.*)'\n*/  
-    file = File.new(args, "r")
-    while(line = file.gets)
-      if(line.match(titleReg))
-        createNewClass($1)
-      end
-      if(line.match(attributeReg))
-        createNewAttribute($1 => $2)
-      end
-      if(line.match(constraintReg))
-        createConstraints($1 => $2)
-      end
+    
+    def Model.generate(filename)
+      @newClass = new
+      @newClass.instance_eval(File.read(filename))
+			create_load_from_file(filename)
+			return @newClass
+			
     end
-    file.close
-    makeConstraintsMethod()
-    return self
-  end
-  
-
-
-  def load_from_file(file)
-    attributes = []
-    values = []
-    ret = []
-    f = YAML.load_file(file)
-      f.each do |key, args|
-      @arrayName = key
-        args.each do |a|
-          a.each do |key , value|
-            attributes << key
-            values << value
-          end 
-          ret << @myClass.new(attributes, values)
-          attributes = []
-          values = []          
-        end
-      end
-      return ret
-  end
-
-  def createNewClass(args)
-    @myClass = Object.const_set(args,Class.new)
-    @myClass.class_eval %{def self.load_from_file(fileName)
-      YAML.load_file(fileName)
-      end}
-    @myClass.class_eval %{def initialize(attri, vali)
-                           for i in 1..attri.size() do
-                           self.method("#\{attri[i-1]\}=").call(vali[i-1])
-                           end
-                         end}
-                         #self.method(#{attri[0]}).call(vali[0])
-  end
-
-  def createNewAttribute(args)
-    set_attr_reader("#{args.keys}")
-    set_attr_writer("#{args.keys}","#{args.values}")
-    @myClass.class_eval %{class_variable_set :@@const, Array.new}
-  end
-
-  def createConstraints(args)
-    @myClass.class_eval %{@@const << args} 
-  end
-
-  def set_attr_writer(sym, type)
-    @myClass.class_eval %{def #{sym}=(val)
-    if(val.kind_of? #{type})
-      if(checkConstraint(:#{sym}, val))
-        @#{sym} = val
-      else
-        raise "Invalid value"
-      end
-    else
-      raise "Type Error"
+    
+    def Model.create_load_from_file(filename)
+      @newClass.instance_eval %{
+                  def load_from_file(filename)
+										@myClass.load_from_file(filename)
+									end}
+									
     end
-  end}    
-end
-
-def set_attr_reader(syms)
-  syms.each do|sym|
-    @myClass.class_eval %{def #{sym}
-    @#{sym}
-  end}
-end
-end
-
-def makeConstraintsMethod()
-  @myClass.class_eval %{def checkConstraint(attribute, value_to_be_checked)
-    valid = true
-    @@const.each do |p|
-      p.each_pair do |key, value|
-        key.each do |k|
-          if(k.to_s == attribute.to_s)
-            if(value.to_s.eql?("! = nil"))
-              if(!(value_to_be_checked != nil))
-                valid = false
-                break
-              end
-            end
-            if(value.to_s.eql?("size > 0"))
-              if(!(value_to_be_checked.size > 0))
-                valid = false
-                break
-              end
-            end
-            if(value.to_s.eql?("=~ /^[A-Z]/"))
-              if(!(value_to_be_checked.match(/[A-Z]/)))
-                valid = false
-                break
-              end
-            end
-            if(value.to_s.eql?(">= 0"))
-              if(!(value_to_be_checked >= 0))
-                valid = false
-              end
-            end
-          end
-        end
-      end
+    
+    def load_from_file(filename)
+      @myClass.class_eval %{
+									def self.load_from_file(filename)
+									  attributes = []
+									  ret = []									
+										attributes = YAML::load(File.open(filename)).values[0]
+										attributes.each do |n|
+											begin
+												obj = self.new(n)
+												ret << obj
+											rescue
+											end
+										end
+										return ret
+									end}
     end
-    return valid 
-  end}
-end  
+    
+        
+    def title(sym)
+		@myClass = Object.const_set(sym, Class.new)
+		load_from_file(sym)
+		@myClass.class_eval %{
+									def initialize(args)
+										args.each do |arg|
+											self.method("#\{arg[0]\}=").call(arg[1])
+										end
+									end
+									}
+    end
+    
+    def set_attr_reader(sym)
+      @myClass.class_eval %{def #{sym}
+                        @#{sym}
+                     end}
+    end
+
+	def constraint(sym, args)
+		@myClass.class_eval %{
+									@@#{sym}_constraints << %(#{args})
+									}
+	end
+    
+    def attribute(sym, args)
+      set_attr_writer(sym, args)
+      set_attr_reader(sym)
+		@myClass.class_eval %{
+									class_variable_set :@@#{sym}_constraints, Array.new
+									def self.#{sym}_constraints
+										@@#{sym}_constraints
+									end
+									def #{sym}_constraints
+										@@#{sym}_constraints
+									end
+									}
+
+    end
+
+   def set_attr_writer(sym, args)
+        @myClass.class_eval %{def #{sym}= (val)
+		  						if val.kind_of? #{args}
+		  							old_#{sym} = @#{sym}
+									@#{sym} = val
+									if @@#{sym}_constraints.nil?
+									  puts "No set constraints"
+									else 
+										@@#{sym}_constraints.each do |con|
+											if !eval(con) || @#{sym} == nil
+												@#{sym} = old_#{sym}
+												raise "Invalid value!"
+											end
+										end
+									end
+								else
+										raise "Invalid type!"
+								end
+                     end
+       }
+   end
+   
 end
