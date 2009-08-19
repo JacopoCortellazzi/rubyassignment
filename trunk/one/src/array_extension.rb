d03a1b9deda4f4e51b30cbe8b97d02d341fe79c1
linus.harling <linus.harling@490d674c-2f14-11de-8e0c-53a27eea117e>
2009-08-19 11:05:00 +0000

    bara borttagen debug

--- trunk/one/src/array_extension.rb
+++ trunk/one/src/array_extension.rb
@@ -1,7 +1,4 @@
 class Array
-	#def select_first(args)
-	#	select(:first => args)
-	#end
 
 	def select(args)
 		retarr = []
@@ -13,7 +10,6 @@ class Array
 					if (element.send(params.values[0]) >= params[:interval][:min] &&
 						element.send(params.values[0]) <= params[:interval][:max])
 						if args[:all]
-							#p (args.keys[0])
 							retarr << element
 						else
 							return element
@@ -22,7 +18,6 @@ class Array
 				else
 					if element.send(params.values[0]) <= params[:interval][:max]
 						if args[:all]
-							#p (args.keys[0])
 							retarr << element
 						else
 							return element
@@ -31,11 +26,9 @@ class Array
 				end
 
 			elsif params.values[0].class == Array
-				#puts ('ALL: ARRAY')
 				params.values[0].each do |value|
 					if value == element.send(params.keys[0].to_s)
 						if args[:all]
-							#p (args.keys[0])
 							retarr << element
 						else
 							return element
@@ -43,34 +36,19 @@ class Array
 					end
 				end
 			else
-				#puts('ALL: SINGLE')
-				puts('key: '+params.keys[0].to_s+', value: '+params.values[0].to_s)
 				if element.send(params.keys[0].to_s) == params.values[0]
 					if args[:all]
 						retarr << element
-						print('SINGLE inlooop ')
-						p(retarr)
 					else
 						return element
 					end
 				end
 			end
 		end
-		#print('calling args: ')
-		#p(args)
-		print('return: ')
-		p(retarr)
 		return retarr
 	end
-
-	#def select_all(args)
-	#	return select(:all => args)
-	#end
 	
 	def method_missing(method, *args)
-		#puts('DEBUG: bla: '+method.to_s)
-		#p(*args)
-		#p(args)
 		if method.to_s.match(/^select_first$/)
 			eval %{def #{method.to_s}(params)
 					return select(:first => params)
@@ -82,23 +60,16 @@ class Array
 				end
 			}
 		elsif method.to_s.match(/^select_(.*)_where_(.*)_is$/)
-			p('DEBUG: case two: ')
-			p(*args)
 			eval %{def #{method.to_s}(params)
-#					puts "$1 = "+"#{$1}"
-#					puts "$2 = "+"#{$2}"
-#					puts "params = "+params
-#					p(self)
-					#TODO här är (var) felet!
-         		    return select("#{$1}".to_sym => {"#{$2}" => params})
+         		    return select("#{$1}".to_sym => {"#{$2}".to_sym => params})
 				end
 			}
 		elsif method.to_s.match(/^select_(.*)_where_(.*)_is_in$/)
 		  eval %{def #{method.to_s}(*params)
 		  			if params.length == 1
-	         		return select("#{$1}" => {:name => "#{$2}", :interval => {:max => params[0]}})
+	         		return select("#{$1}".to_sym => {:name => "#{$2}".to_sym, :interval => {:max => params[0]}})
 					elsif params.length == 2
-	         		return select("#{$1}" => {:name => "#{$2}", :interval => {:min => params[0], :max => params[1]}})
+	         		return select("#{$1}".to_sym => {:name => "#{$2}".to_sym, :interval => {:min => params[0], :max => params[1]}})
 					end
 				end
 		  }
